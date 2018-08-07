9d80d6e5e723aad4ad895fc3218e27d5bc2f6c82
doweryouxia <1306068431@qq.com>
2018-08-07 18:09:40 +0800

    权限接口提交

--- 01.项目源码/trunk/0.0.1/shareride-admin/src/main/java/com/dower/sharerideadmin/controller/admin/MemberController.java
+++ 01.项目源码/trunk/0.0.1/shareride-admin/src/main/java/com/dower/sharerideadmin/controller/admin/MemberController.java
@@ -23,11 +23,61 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
+    /**
+     * 获取权限详情
+     * @param nnaRights
+     * @return
+     */
+    @RequestMapping(value = "/getOneRight")
+    public Result getOneRight(NnaRights nnaRights){
+        return memberService.getOneRight(nnaRights);
+    }
+
+    /**
+     * 菜单权限修改
+     * @param nnaRights
+     * @return
+     */
+    @RequestMapping(value="/updateRight")
+    public Result updateRight(NnaRights nnaRights){
+        return memberService.updataRight(nnaRights);
+    }
+
+    /**
+     * 菜单权限添加
+     * @param nnaRights
+     * @return
+     */
+    @RequestMapping(value="/addRight")
+    public Result addRight(NnaRights nnaRights){
+        return memberService.addRight(nnaRights);
+    }
+
+    /**
+     *权限列表分页查询
+     * @param nnaRights
+     * @param currentPage
+     * @param pageSize
+     * @return
+     */
     @RequestMapping(value="/getRightPage")
-    public Result getRightPage(NnaRights nnaRights, int currentPage, int pageSize) {
+    public Result getRightPage(NnaRights nnaRights, Integer currentPage, Integer pageSize) {
+        if(currentPage == null){
+            currentPage = 1;
+        }
+        if(pageSize == null){
+            pageSize = 0;
+        }
         Result pageRight = memberService.getPageRight(nnaRights, currentPage, pageSize);
         return pageRight;
     }
+
+    /**
+     * 测试：用户分页查询
+     * @param currentPage
+     * @param pageSize
+     * @return
+     */
     @RequestMapping(value="/getPageUser")
     public Result getPageUser(int currentPage, int pageSize) {
         Result pageRight = memberService.getPageUser( currentPage, pageSize);
