81d0556f84f0461b7a74751af7acbd66ad234f56
doweryouxia <1306068431@qq.com>
2019-12-06 00:11:51 +0800

    add redisson setting.

--- 01.项目源码/trunk/0.0.1/shareride-app/src/test/java/com/dower/sharerideapp/AppApplicationTests.java
+++ 01.项目源码/trunk/0.0.1/shareride-app/src/test/java/com/dower/sharerideapp/AppApplicationTests.java
@@ -1,16 +1,38 @@
 package com.dower.sharerideapp;
 
+import com.dower.sharerideapp.service.RedisService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class AppApplicationTests {
+	@Autowired
+	RedisTemplate redisTemplate;
+
+	@Autowired
+	RedisService redisService;
 
 	@Test
 	public void contextLoads() {
+		Boolean test01 = redisTemplate.hasKey("test01");
+		log.info("======test01========="+test01);
+		redisTemplate.opsForValue().set("name","李月芳");
+		Boolean name1 = redisTemplate.hasKey("name");
+		log.info("======name1========="+name1);
+		String name = redisTemplate.opsForValue().get("name").toString();
+		log.info("======name========="+name);
+	}
+
+	@Test
+	public void testRedis01(){
+
 	}
 
 }
