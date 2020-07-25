52376ec4bebf76a6b2efd50b4a8e3c43b596aab3
doweryouxia <1306068431@qq.com>
2020-07-25 16:59:36 +0800

    123

--- 01.项目源码/trunk/0.0.1/clothing-server/src/test/java/com/dower/sharerideapp/service/UsersServiceTest.java
+++ 01.项目源码/trunk/0.0.1/clothing-server/src/test/java/com/dower/sharerideapp/service/UsersServiceTest.java
@@ -1,9 +1,17 @@
 package com.dower.sharerideapp.service;
 
+import com.dower.sharerideapp.core.serverdb.dao.NntUsersMapper;
 import com.dower.sharerideapp.core.serverdb.model.NntUsers;
+import com.dower.sharerideapp.core.serverdb.model.NntUsersExample;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +19,24 @@ import static org.junit.Assert.*;
  * Created by wangwei on 2020/7/6.
  */
 @Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UsersServiceTest {
+    @Autowired
+    private NntUsersMapper nntUsersMapper;
+    @Test
+    public void test() throws Exception{
+        NntUsersExample example = new NntUsersExample();
+        NntUsersExample.Criteria criteria = example.createCriteria();
+        criteria.andVcUnionidEqualTo("oDtaUw-SYC5fRkzQSmqx_wZ6Wyk0");
+        List<NntUsers> Users = nntUsersMapper.selectByExample(example);
 
+        NntUsers nntUsers = Users.get(0);
+        String vcHeadImgUrl = nntUsers.getVcHeadImgUrl();
+        if(null==vcHeadImgUrl|| StringUtils.isBlank(vcHeadImgUrl)){
+            log.info("微信头像获取结果为：：{}",vcHeadImgUrl);
+        }
+        log.info("查询结果返回="+vcHeadImgUrl);
+    }
 
 }
